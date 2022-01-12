10954938c1b6efefd584bdc6aca814c76fdb2a5d
ecommerce <rookaa20149@gmail.com>
2022-01-11 21:07:00 -0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import '@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css';
+import './css/style.css';
+import 'jquery/dist/jquery.min';
+import 'popper.js/dist/popper.min';
+import 'bootstrap/dist/js/bootstrap.min.js';
