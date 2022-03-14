09c068f1707454498a850f725abb1dcef75d1df8
ecommerce <rookaa20149@gmail.com>
2022-03-14 04:40:04 -0700

    carousel featured

--- src/index.js
+++ src/index.js
@@ -23,4 +23,105 @@ $(function(){
         $(this).parents('.product-option').siblings().removeClass('active');
         $(this).parents('.product-option').addClass('active');
     });
+
+    // عندما تتغير كمية المنتج
+  $('[data-product-quantity]').on( "change",function() {
+      
+    // اجلب الكمية الجديدة
+    var newQuantity = $(this).val();
+
+    // ابحث عن السّطر الّذي يحتوي معلومات هذا المُنتج
+    var $parent = $(this).parents('[data-product-info]');
+
+    // اجلب سعر القطعة الواحدة من معلومات المنتج
+    var pricePerUnit = $parent.attr('data-product-price');
+
+    // السعر الإجمالي للمنتج هو سعر القطعة مضروبًا بعددها
+    var totalPriceForProduct = newQuantity * pricePerUnit;
+
+    // عين السعر الجديد ضمن خليّة السّعر الإجمالي للمنتج في هذا السطر
+    $parent.find('.total-price-for-product').text(totalPriceForProduct + '$');
+
+   // حدث السعر الإجمالي لكل المُنتجات
+   calculateTotalPrice();
 });
+
+$('[data-remove-from-cart]').click(function(){
+    $(this).parents('[data-product-info]').remove();
+    //أعد حساب السعر الإجمالي بعد حذف المنتجات
+    calculateTotalPrice();
+});
+function calculateTotalPrice() {
+    
+  // أنشئ متغيّرًا جديدًا لحفظ السعر الإجمالي
+  var totalPriceForAllProducts = 0;
+
+  // لكل سطر يمثل معلومات المُنتج في الصّفحة
+  $('[data-product-info]').each(function() {
+
+      // اجلب سعر القطعة الواحدة من الخاصّية الموافقة
+      var pricePerUnit = $(this).attr('data-product-price');
+
+      // اجلب كمية المنتج من حقل اختيار الكمية
+      var quantity = $(this).find('[data-product-quantity]').val();
+
+      var totalPriceForProduct = pricePerUnit * quantity;
+
+      // أضف السعر الإجمالي لهذا المنتج إلى السعر الإجمالي لكل المُنتجات، واحفظ القيمة في المتغير نفسه
+      totalPriceForAllProducts = totalPriceForAllProducts + (totalPriceForProduct);
+      
+  });
+
+    // حدث السعر الإجمالي لكل المُنتجات في الصفحة
+  $('#total-price-for-all-products').text(totalPriceForAllProducts + '$');
+}
+var citiesByCountry = {
+    sa: ['الرياض','جدة'],
+    eg: ['القاهرة','الإسكندرية'],
+    jo: ['عمان','الزرقاء'],
+    sy: ['دمشق','حلب','حماه']
+};
+     // عندما يتغير البلد
+  $('#form-checkout select[name="country"]').on( "change",function() {
+      // اجلب رمز البلد
+    var country = $(this).val();
+    // اجلب مدن هذا البلد من المصفوفة
+    var cities = citiesByCountry[country];
+    // فرّغ قائمة المدن
+    $('#form-checkout select[name="city"]').empty();
+    //إضافة خيار اختر مدينه
+    $('#form-checkout select[name="city"]').append(
+        '<option disabled selected value="">اختر المدينة</option>'
+    );
+     // أضف المدن إلى قائمة المدن
+     cities.forEach(function(city) {
+        var newOption = $('<option></option>');
+        newOption.text(city);
+        newOption.val(city);
+        $('#form-checkout select[name="city"]').append(newOption);
+      });
+
+  });
+  // عندما تتغير طريقة الدفع
+  $('#form-checkout input[name="payment_method"]').change(function() {
+
+    // اجلب القيمة المُختارة حاليًا
+    var paymentMethod = $(this).val();
+
+    if (paymentMethod === 'on_delivery') {
+
+      // إذا كانت عند الاستلام، فعطّل حقول بطاقة الائتمان
+      $('#credit-card-info input').prop('disabled', true);
+
+    } else {
+
+      // وإلا ففعلّها
+      $('#credit-card-info input').prop('disabled', false);
+    }
+  
+    // بدل معلومات بطاقة الائتمان بين الظهور والإخفاء
+    $('#credit-card-info').toggle();
+  });
+  
+});
+
--- webpack.config.js
+++ webpack.config.js
@@ -97,6 +97,15 @@ module.exports = {
         filename: "product.html",
         template: "./src/product.html",
     }),
+
+    new HtmlWebpackPlugin({
+        filename: "checkout.html",
+        template: "./src/checkout.html",
+    }),
+    new HtmlWebpackPlugin({
+        filename: "payment.html",
+        template: "./src/payment.html",
+    }),
     new MiniCssExtractPlugin({filename: "css/style.css"}),
     new OptimizeCSSAssetsPlugin({}),
 
